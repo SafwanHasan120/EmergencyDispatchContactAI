afdc4524f18ee8d46663ec5a0f6ffa4dfff26534
SafwanHasan120 <safwanhasan120@outlook.com>
2024-07-13 15:37:51 -0700

    created center widgets, no functionality yet

--- website/dispatchai/src/App.js
+++ website/dispatchai/src/App.js
@@ -7,9 +7,24 @@ function App() {
     <div class="container">
       <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@400;700&family=Outfit:wght@400;700&display=swap" rel="stylesheet"></link>
       <div class="title" href="/">DispatchAI</div>
+      <div class="widget-container">
+        <div class="assist">Possible ways for me to assist:</div>
+        <div class="widgets">
+          <div class="widget">
+            <a href="#">Medical Situation Support</a>
+          </div>
+          <div class="widget">
+            <a href="#">Mental Health Crisis</a>
+          </div>
+          <div class="widget">
+            <a href="#">General Emergency Support</a>
+          </div>
+        </div>
+      </div>
       <div class="input-container">
         <textarea type="text" class="input" placeholder="Message DispatchAI"></textarea>
       </div>
+
       
     </div>
 
